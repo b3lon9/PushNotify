3dbf701bcb1e5b9118208e339c6ef68273a3a67b
neander <b3xlon9@gmail.com>
2023-11-24 10:01:43 +0900

    토큰 확인 및 메시지 도착 로그 확인

--- FirebaseCloudMessagingApp/app/src/main/java/kr/co/cavedwellers/firebasecloudmessagingapp/MainActivity.kt
+++ FirebaseCloudMessagingApp/app/src/main/java/kr/co/cavedwellers/firebasecloudmessagingapp/MainActivity.kt
@@ -3,27 +3,50 @@ package kr.co.cavedwellers.firebasecloudmessagingapp
 import android.Manifest
 import android.content.pm.PackageManager
 import android.os.Build
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.result.contract.ActivityResultContracts
+import androidx.appcompat.app.AppCompatActivity
 import androidx.core.content.ContextCompat
+import com.b3lon9.nlog.NLog
 import com.google.android.gms.tasks.OnCompleteListener
-import com.google.android.gms.tasks.Task
+import com.google.firebase.ktx.Firebase
 import com.google.firebase.messaging.FirebaseMessaging
+import com.google.firebase.messaging.ktx.messaging
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        // FCM이 자동 초기화 설정
+        // Firebase.messaging.isAutoInitEnabled = true      // 다시 자동 초기화됨
+
         askNotificationPermission()
+        checkToken()
     }
 
+    override fun onResume() {
+        super.onResume()
+
+        // Google Play 서비스 확인
+    }
+
+    // 현재 등록 토큰 가져오기
     private fun checkToken() {
-        FirebaseMessaging.getInstance().token.addOnCompleteListener(object: OnCompleteListener<String> {
-            override fun onComplete(p0: Task<String>) {
-                TODO("Not yet implemented")
+        FirebaseMessaging.getInstance().token.addOnCompleteListener(OnCompleteListener {task ->
+            if (!task.isSuccessful) {
+                NLog.w("checkToken.. registration token failed.. exception:${task.exception?.stackTraceToString()}")
+                return@OnCompleteListener
             }
+
+            // Get new FCM registration token
+            val token = task.result
+
+            // Log and toast
+            val msg = "토큰:$token"
+            NLog.d("checkToken.. token:$msg")
+            Toast.makeText(this, msg, Toast.LENGTH_SHORT).show()
         })
     }
 
--- FirebaseCloudMessagingApp/app/src/main/java/kr/co/cavedwellers/firebasecloudmessagingapp/serivce/MyFirebaseMessagingService.kt
+++ FirebaseCloudMessagingApp/app/src/main/java/kr/co/cavedwellers/firebasecloudmessagingapp/serivce/MyFirebaseMessagingService.kt
@@ -1,7 +1,27 @@
 package kr.co.cavedwellers.firebasecloudmessagingapp.serivce
 
+import com.b3lon9.nlog.NLog
 import com.google.firebase.messaging.FirebaseMessagingService
+import com.google.firebase.messaging.RemoteMessage
 
 class MyFirebaseMessagingService: FirebaseMessagingService() {
+    override fun onCreate() {
+        super.onCreate()
+        NLog.v("onCreate..")
+    }
 
+    override fun onNewToken(token: String) {
+        super.onNewToken(token)
+        NLog.d("onNewToken.. token:$token")
+    }
+
+    private fun sendRegistrationToServer(token: String) {
+
+    }
+
+    override fun onMessageReceived(message: RemoteMessage) {
+        super.onMessageReceived(message)
+        NLog.v("onMessageReceived.. message:${message}")
+        NLog.v("onMessageReceived.. message:${message.data}")
+    }
 }
